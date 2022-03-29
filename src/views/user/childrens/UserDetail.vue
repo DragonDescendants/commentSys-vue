ca03798213c44697a196aad9e12f7f5cc8ba4dce
master <873691128@qq.com>
2022-03-29 22:36:59 +0800

    2022/03/29

--- src/views/user/childrens/UserDetail.vue
+++ src/views/user/childrens/UserDetail.vue
@@ -13,68 +13,71 @@
       </template>-->
     </el-result>
   </div>
- 
+
   <div v-else>
-      <h2 style="text-align: center;">个人资料</h2>
-      <div v-if="rt.query.id == store.userId" style="display: flex;">
-        <div style="display: flex;flex: 1;text-align: left;">
-          <el-form label-position="left" label-width="100px" :model="user" style="width: 460px">
-            <el-form-item label="姓名">
-              <el-input v-model.trim="user.name"></el-input>
-            </el-form-item>
-            <el-form-item label="性别">
-              <!-- <el-input v-model.trim="user.sex"></el-input> -->
-              <el-radio-group v-model="user.sex">
-                <el-radio :label="0">不愿意透露</el-radio>
-                <el-radio :label="1">男</el-radio>
-                <el-radio :label="2">女</el-radio>
-              </el-radio-group>
-            </el-form-item>
-            <el-form-item label="...">...</el-form-item>
-            <el-form-item label="...">...</el-form-item>
-            <el-form-item label="...">...</el-form-item>
-            <el-form-item label="年龄">
-              <el-input v-model.number="user.age"></el-input>
-              <!-- <el-input-number v-model="user.age" :min="1" :max="200" @change="handleChange" /> -->
-              <!-- <el-input-number v-model="user.age" :min="1" :max="200"/> -->
-            </el-form-item>
-            <el-button type="primary" :disabled="!isChange" @click="resetChange">重置</el-button>
-            <el-button type="primary" :disabled="!isChange" @click="updateUserDetail">提交</el-button>
-          </el-form>
-        </div>
-        <div v-if="store.userId !== 0" style="display: flex; flex: 1;text-align: right;">
-          <el-space direction="vertical" alignment="flex-start" style="margin-top: 10px;">
-            <div style="font-size: small;">
-              头像
-              <el-button class="ml-3" type="success" @click="submitUpload">确认上传</el-button>
+    <h2 style="text-align: center;">个人资料</h2>
+    <div v-if="rt.query.id == store.userId" style="display: flex;">
+      <div style="display: flex;flex: 1;text-align: left;">
+        <el-form label-position="left" label-width="100px" :model="user" style="width: 460px">
+          <el-form-item label="姓名">
+            <el-input v-model.trim="user.name"></el-input>
+          </el-form-item>
+          <el-form-item label="性别">
+            <!-- <el-input v-model.trim="user.sex"></el-input> -->
+            <el-radio-group v-model="user.sex">
+              <el-radio :label="0">不愿意透露</el-radio>
+              <el-radio :label="1">男</el-radio>
+              <el-radio :label="2">女</el-radio>
+            </el-radio-group>
+          </el-form-item>
+          <el-form-item label="...">...</el-form-item>
+          <el-form-item label="...">...</el-form-item>
+          <el-form-item label="...">...</el-form-item>
+          <el-form-item label="年龄">
+            <el-input v-model.number="user.age"></el-input>
+            <!-- <el-input-number v-model="user.age" :min="1" :max="200" @change="handleChange" /> -->
+            <!-- <el-input-number v-model="user.age" :min="1" :max="200"/> -->
+          </el-form-item>
+          <el-button type="primary" :disabled="!isChange" @click="resetChange">重置</el-button>
+          <el-button type="primary" :disabled="!isChange" @click="updateUserDetail">提交</el-button>
+        </el-form>
+      </div>
+      <div v-if="store.userId !== 0" style="display: flex; flex: 1;flex-direction: column;">
+        <div style="text-align: center;">
+          <div style="font-size: medium;">头像</div>
+          <el-tooltip effect="dark" placement="top" content="头像支持jpg/png/gif格式,大小应在1MB以内">
+            <div style="margin-top: 20px;" @click="selectImage">
+              <el-avatar v-if="fileShowUrl" :src="fileShowUrl" :size="200" />
+              <el-avatar v-else :size="200">选择头像文件</el-avatar>
             </div>
-            <el-upload
-              :headers="headers"
-              ref="uploadRef"
-              class="avatar-uploader"
-              action="http://localhost:8888/file/upload"
-              :show-file-list="false"
-              :on-success="handleAvatarSuccess"
-              :before-upload="beforeAvatarUpload"
-              :auto-upload="false"
-              style="width: 100px;height: 100px;"
-            >
-              <img v-if="imageUrl" :src="imageUrl" class="avatar" />
-              <el-icon v-else class="avatar-uploader-icon">
-                <plus />
-              </el-icon>
-            </el-upload>
-          </el-space>
+          </el-tooltip>
+          <input
+            v-show="false"
+            ref="uploadInput"
+            type="file"
+            class="dl-none"
+            name="icon"
+            @change="onImageFileChange"
+          />
+          <el-button
+            type="primary"
+            :disabled="!fileShowUrl"
+            style="margin-top: 20px;"
+            @click="uploadSubmit"
+          >
+            <span>上传</span>
+          </el-button>
+          <!-- <div style="margin-top: 10px;">支持扩展名：.jpg .png .gif</div> -->
         </div>
       </div>
-      <div v-else>这不是你！！！</div>
+    </div>
+    <div v-else>这不是你！！！</div>
   </div>
- 
 </template>
 
 <script setup>
 import { ElMessage } from 'element-plus';
-import { reactive, ref, watch,inject } from 'vue'
+import { reactive, ref, watch, inject } from 'vue'
 import { useRoute } from 'vue-router';
 import http from '../../../api/api';
 import { useStore } from '../../../store/store'
@@ -128,51 +131,76 @@ const imageUrl = ref('')
 // 图片上传组件的ref
 const uploadRef = ref()
 // 图片上传请求的header
-const headers = {
-  Authorization: Cookies.get("token")
-}
 // 上传结束后的回调
-const handleAvatarSuccess = (res, file) => {
-  console.log(res)
-  if (res !== 'failed') {
-    http.put('/user/img', {
-      id: store.userId,
-      img: res
-    }).then((response) => {
-      if (response.data.code === 200) {
+// 传输文件前的校验
+// 上传请求的函数
+// 提交上传的函数
+
+// 存放file对象的数组
+const fileObjectArray = ref(null)
+const fileShowUrl = ref(null)
+// 上传框的ref对象
+const uploadInput = ref(null);
+// 上传框放入了头像
+const onImageFileChange = (e) => {
+  const input = e.target;
+  fileObjectArray.value = input.files;
+  if (fileObjectArray.value) {
+    const currentFile = fileObjectArray.value[0]
+    const isImg = currentFile.type === 'image/jpeg' || currentFile.type === 'image/png' || currentFile.type === 'image/gif'
+    const isLess1MB = currentFile.size / 1024 < 1024
+    if (isImg && isLess1MB) {
+      console.log(currentFile)
+      const fileReader = new FileReader()
+      fileReader.readAsDataURL(currentFile);
+      fileReader.onload = () => {
+        fileShowUrl.value = fileReader.result
+      }
+    } else {
+      fileObjectArray.value = null
+      ElMessage({
+        type: "warning",
+        message: "文件参数不合法,应为jpg/png/gif格式,且大小在1MB以内"
+      })
+      input.files = null
+      input.value = null
+    }
+  }
+}
+//选择头像
+const selectImage = () => {
+  console.log(uploadInput.value)
+  let oBtn = uploadInput.value;
+  oBtn.click();
+}
+// 上传头像
+const uploadSubmit = () => {
+  if (fileObjectArray.value) {
+    let formdata = new FormData()
+    formdata.append('userId', store.userId)
+    formdata.append('file', fileObjectArray.value[0])
+    http.post('/file/userImg/upload', formdata, {
+      headers: {
+        'Content-Type': 'multipart/form-data'
+      }
+    }).then((res) => {
+      if (res.data.code === 200) {
+        console.log("成功上传!")
+        fileObjectArray.value = null
         ElMessage({
-          message: "上传头像成功",
           type: "success",
+          message: "用户头像已成功更改,请刷新"
         })
-        store.setImage(res)
+      } else {
+        ElMessage({
+          type: "error",
+          message: "头像上传失败,请重试"
+        })
       }
     })
   }
-  imageUrl.value = URL.createObjectURL(file.raw)
-}
-// 传输文件前的校验
-const beforeAvatarUpload = (file) => {
-  const isJPG = file.type === 'image/jpeg'
-  const isLt1M = file.size / 1024 < 1024
-
-  if (!isJPG) {
-    ElMessage.error('Avatar picture must be JPG format!')
-  }
-  if (!isLt1M) {
-    ElMessage.error('Avatar picture size can not exceed 1MB!')
-  }
-  return isJPG && isLt1M
-}
-// 上传请求的函数
-const imageUpload = () => {
-
-}
-// 提交上传的函数
-const submitUpload = () => {
-  uploadRef.value.submit()
 }
 
-const count = ref(0)
 
 const getUserDetail = () => {
   if (id)
@@ -196,14 +224,14 @@ const getUserDetail = () => {
         loading.value = false
         notFound.value = true
       }
-    }).catch((error) => {
+    }).catch(() => {
       loading.value = false
     })
 }
 //创建时加载一次
 getUserDetail()
 
-watch(() => user, (oldValue, newValue) => {
+watch(() => user, () => {
   if (loading.value === true) {
     isChange.value = false
   }
